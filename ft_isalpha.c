1f6dd3ce7f0b5b064ca2e1aa0990fe0927e5d6b6
Undimmer <145223362+undimmer@users.noreply.github.com>
2023-11-24 19:45:50 +0100

    Create ft_isalpha.c

---
+++ ft_isalpha.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anquesad <anquesad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 12:29:06 by anquesad          #+#    #+#             */
+/*   Updated: 2023/11/24 19:02:25 by anquesad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// Verifica si un carácter es alfabético
+
+int	ft_isalpha(int c)                                         // Toma como argumento 'c'
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))       // Aquí verifica si el valor de 'c' está dentro del rango del alfabeto  
+		return (1);                                               // Devolverá 1 si indica que es una letra o un 0 indicando que el caracter no es una letra
+	return (0);
+}
