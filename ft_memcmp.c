8c44e62ef473a49b25bd829d373d89fbf4b5bfab
Undimmer <145223362+undimmer@users.noreply.github.com>
2024-03-30 18:41:39 +0100

    Create ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anquesad <anquesad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 13:57:36 by anquesad          #+#    #+#             */
+/*   Updated: 2023/11/05 16:19:05 by anquesad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t				i;
+	unsigned const char	*str1;
+	unsigned const char	*str2;
+
+	i = 0;
+	str1 = (unsigned const char *)s1;
+	str2 = (unsigned const char *)s2;
+	while (i < n)
+	{
+		if (*str1 != *str2)
+		{
+			return ((int)(*str1 - *str2));
+		}
+		str1++;
+		str2++;
+		i++;
+	}
+	return (0);
+}
