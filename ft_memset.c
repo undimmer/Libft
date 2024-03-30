c6c266c5b17a7fa7aae83d90be715224420f5b99
Undimmer <145223362+undimmer@users.noreply.github.com>
2024-03-30 18:42:42 +0100

    Create ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anquesad <anquesad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/24 11:51:15 by anquesad          #+#    #+#             */
+/*   Updated: 2023/11/24 19:10:04 by anquesad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *ptr, int x, size_t n)
+{
+	size_t			i;
+	unsigned char	*s;
+
+	i = 0;
+	s = ptr;
+	while (i < n)
+	{
+		s[i] = (unsigned char)x;
+		i++;
+	}
+	return (ptr);
+}
